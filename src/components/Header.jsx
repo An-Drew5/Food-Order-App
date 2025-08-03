97401f29cb172fe8259d612701e97018a03fd3ed
An-Drew5 <andrewoppong123@gmail.com>
2025-08-03 22:20:42 +0100

    Daily Update: Fetched items from backend and did some styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button';
+
+export default function Header() {
+    return <header id="main-header">
+        <div id="title">
+            <img src={logoImg} alt="Logo" />
+            <h1>Big Mike's Eatary</h1>
+        </div>
+        <nav>
+            <Button textOnly>Cart (0)</Button>
+        </nav>
+    </header>
+}
---
+++ src/util/formatting.js
@@ -0,0 +1,4 @@
+export const currencyFormatter = new Intl.NumberFormat('en-GH', {
+    style: 'currency',
+    currency: 'GHC',
+})
